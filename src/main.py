1b3b6c2eebb55053b8a5d1a10d16bec37feec513
mshrynzw <iq87io25@gmail.com>
2024-09-11 10:24:07 +0900

    Add: yfinance

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from common import set_log, set_common
 from logging import getLogger, config
+import yfinance as yf
 
 
 log_conf = set_log()
@@ -10,4 +11,11 @@ logger = getLogger(__name__)
 common_conf = set_common()
 
 if __name__ == '__main__':
-    logger.info(common_conf['AAA'])
+    usdjpy = yf.Ticker("USDJPY=X")
+    data = usdjpy.history(period="3mo")
+    logger.info("Open:\n{}\nClose:\n{}\nHigh:\n{}\nLow:\n{}".format(
+        data['Open'].to_string(),
+        data['Close'].to_string(),
+        data['High'].to_string(),
+        data['Low'].to_string()
+    ))
